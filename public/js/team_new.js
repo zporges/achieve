f3fc77ce7ca8ebdc4716dbe078b5cf0a83670287
zporges <porges@erdhcp-10-33-130-178.eduroam.cornell.edu>
2014-04-24 17:26:27 -0400

    dont try again yet

---
+++ public/js/team_new.js
@@ -0,0 +1,30 @@
+function addUser(){
+	  $("[name=error"+parseInt($("#numUsers").attr("value"),10)+"]").html("");
+	  $("#numUsers").attr("value", parseInt($("#numUsers").attr("value"),10) + 1);
+	  $("#addUser").append( '<input type="text" name = "user'+$("#numUsers").attr("value")+'" placeholder="Enter a teammates\'s email" class= "addUser" />');
+	  $("#addUser").append( '<p class ="error" name= "error'+$("#numUsers").attr("value")+'"></p>');
+}
+
+function validateEmail(email){
+	var atpos=email.indexOf("@");
+	var dotpos=email.lastIndexOf(".");
+	if (atpos<1 || dotpos<atpos+2 || dotpos+2>=email.length)
+  {
+    //Not a valid e-mail address
+    return false;
+  }
+	else{
+		//Valid Email
+		return true;	
+	}
+}
+
+$( document ).ready(function() {
+  var now = new Date();
+  var day = ("0" + now.getDate()).slice(-2);
+  var month = ("0" + (now.getMonth() + 1)).slice(-2);
+  var today = (now.getFullYear()+ 1)+"-"+(month)+"-"+(day);
+  if ($('#editDeadline').val() == null || $('#editDeadline').val() == ""){
+  	$('#editDeadline').val(today)
+  };
+});
