aa7eca4021d0ad3fba4ad5500025d52b502debc7
Tre <tsc64@cornell.edu>
2014-04-24 00:47:31 -0400

    adds iphone app

---
+++ iPhone/achieve/Achieve/Achieve/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Achieve
+//
+//  Created by Tre' Sean Calhoun on 4/24/14.
+//  Copyright (c) 2014 GroupGoals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIWebView *viewWeb;
+
+@end
