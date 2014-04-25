f8175797b254b66e6ddaa9f5326a11f25a5280b7
Charles Lai <cjl223@cornell.edu>
2014-04-24 23:35:30 -0400

    fix checkin times and color changes

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -2,16 +2,16 @@ $(document).ready(function(){
   console.log("loaded");
   $(".checkin_box form").addClass("hidden");
 
+  $(".checkin_header h4 .time").each(function(index,element){
+    reformatDate($(element), true);
+  });
+
   $(".main_progress h4 span").each(function(index,element){
     reformatDate($(element), false);
   });
 
   $(".progress_comment p .time").each(function(index, element){
     reformatDate($(element), false);
-  });
-
-  $(".checkin_header h4 .time").each(function(index,element){
-    reformatDate($(element), true);
   });
 
   $(".info .time").each(function(index, element) {
@@ -53,20 +53,24 @@ function expandBox(element, event){
 
 function reformatDate(element, checkin_status){
   var date = element.text().toString().trim();
-  console.log(date);
+  //split at Z
   var split = date.split("Z");
   var passedDate = new Date(split[0]);
   var currentDate = new Date();
   var difference = Date.parse(currentDate) - Date.parse(passedDate);
+  //If checkin_status is true, alter the date logic slightly. Time format is different than comments.
   if (checkin_status == true){
-
+    //split at )
+    var split = date.split(")");
+    var passedDate = new Date(split[0]);
+    var difference = Date.parse(currentDate) - Date.parse(passedDate);
     if (difference < 86400000){
-      console.log(element.parent().prev().prev().attr("class"));
       element.parent().prev().prev().css("background-color", "#61cf80");
       element.parent().prev().prev().find(".fill").css("fill", "#61cf80");
     }
   }
-  else if (difference < 1000){
+  //Parse the time string for all!
+  if (difference < 1000){
     element.text("1 second ago ");
   }
   else if (difference < 60000){
